99d4440f810c3a604df6e89e8c9b0e8529a6b8f6
jenvm16-c <jenivm16@gmail.com>
2026-01-14 20:52:59 +0100

    modificar docker-compose y dockerfile con los nombres del grupo, modificar las rutas en los .py para que no de error el contenedor python y comprobado que se levantan todos los contenedores

--- api/web/rutas_coches.py
+++ api/web/rutas_coches.py
@@ -1,54 +1,42 @@
 from flask import request, Blueprint, jsonify
 import controlador_coches
-from funciones_auxiliares import Encoder
 
 bp = Blueprint('coches', __name__)
 
-@bp.route("/",methods=["GET"])
+@bp.route("/", methods=["GET"])
 def coches():
-    respuesta,code= controlador_coches.obtener_coches()
-    return jsonify(respuesta), code
-    
-@bp.route("/<id>",methods=["GET"])
-def coche_por_id(id):
-    respuesta,code = controlador_coches.obtener_coche_por_id(id)
+    respuesta, code = controlador_coches.obtener_coches()
     return jsonify(respuesta), code
 
-@bp.route("/",methods=["POST"])
+@bp.route("/<int:id>", methods=["GET"])
+def coche_por_id(id):
+    respuesta, code = controlador_coches.obtener_coche_por_id(id)
+    return jsonify(respuesta), code
+
+@bp.route("/", methods=["POST"])
 def guardar_coche():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        coche_json = request.json
-        nombre = coche_json["nombre"]
-        descripcion = coche_json["descripcion"]
-        precio=coche_json["precio"]
-        foto=coche_json["foto"]
-        marca=coche_json["marca"]
-        respuesta,code=controlador_coches.insertar_coche(nombre, descripcion,precio,foto,marca)
+    if request.is_json:
+        data = request.json
+        respuesta, code = controlador_coches.insertar_coche(
+            data["nombre"], data["descripcion"], float(data["precio"]), data.get("foto"), data.get("marca")
+        )
     else:
-        respuesta={"status":"Bad request"}
-        code=401
+        respuesta, code = {"status":"Bad request"}, 401
     return jsonify(respuesta), code
 
 @bp.route("/<int:id>", methods=["DELETE"])
 def eliminar_coche(id):
-    respuesta,code=controlador_coches.eliminar_coche(id)
+    respuesta, code = controlador_coches.eliminar_coche(id)
     return jsonify(respuesta), code
 
 @bp.route("/", methods=["PUT"])
 def actualizar_coche():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        coche_json = request.json
-        id = coche_json["id"]
-        nombre = coche_json["nombre"]
-        descripcion = coche_json["descripcion"]
-        precio=float(coche_json["precio"])
-        foto=coche_json["foto"]
-        marca=coche_json["marca"]
-        respuesta,code=controlador_coches.actualizar_coche(id,nombre,descripcion,precio,foto,marca)
+    if request.is_json:
+        data = request.json
+        respuesta, code = controlador_coches.actualizar_coche(
+            data["id"], data["nombre"], data["descripcion"], float(data["precio"]),
+            data.get("foto"), data.get("marca")
+        )
     else:
-        respuesta={"status":"Bad request"}
-        code=401
+        respuesta, code = {"status":"Bad request"}, 401
     return jsonify(respuesta), code
-
